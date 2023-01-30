ebb41ab044b8c507dae3ea66d99172d97148fb03
Raphaël Macquet <macquetraphael@gmail.com>
2023-01-30 15:41:44 +0100

    End of Django training

--- django/mysite/affiliation/views.py
+++ django/mysite/affiliation/views.py
@@ -1,18 +1,65 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from affiliation.models import Band
+from affiliation.forms import ContactUsForm, BandForm
+from django.core.mail import send_mail
+from django.shortcuts import redirect
 
-def hello(request):
+
+def band_list(request):
     bands = Band.objects.all()
-
-    return HttpResponse(f"""
-    <h1>Hello Django!</h1>
-    <p>Mes groupes préférés sont :<p>
-    <ul>
-        <li>{bands[0].name}</li>
-        <li>{bands[1].name}</li>
-    </ul>
-""")
-
+    return render(request, 
+                'affiliation/band_list.html',
+                {'bands': bands})
 def about(request):
-    return HttpResponse('<h1>A propos</h1> <p>Nous adorons Rapmac !</p>')
+    return render(request,
+                'affiliation/about.html')
+
+def band_detail(request, id):
+    band = Band.objects.get(id=id)
+    return render(request, 'affiliation/band_detail.html', {'band': band})#pass id to model
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactUsForm(request.POST)
+        if form.is_valid():
+            send_mail(
+            subject=f'Message from {form.cleaned_data["name"] or "anonyme"} via MerchEx Contact Us form',
+            message=form.cleaned_data['message'],
+            from_email=form.cleaned_data['email'],
+            recipient_list=['admin@merchex.xyz'],
+        )
+            return redirect('band-list')  # ajoutez cette instruction de retour
+    else:
+        form = ContactUsForm()
+    return render(request,
+                'affiliation/contact.html',
+                {'form':form})
+
+def band_create(request):
+    if request.method == 'POST':
+        form = BandForm(request.POST)
+        if form.is_valid():
+            band = form.save()
+            return redirect('band-detail', band.id)
+    else:
+            form = BandForm()
+    return render(request,
+            'affiliation/band_create.html',
+            {'form':form})
+
+
+def band_update(request, id):
+    band = Band.objects.get(id=id)
+    form = BandForm(instance=band)
+    return render(request, 'affiliation/band_update.html',
+                    {'form':form})
+
+def band_delete(request, id):
+    band = Band.objects.get(id=id)
+    if request.method == 'POST':
+        band.delete()
+        return redirect('band-list')
+    return render(request,
+                    'affiliation/band_delete.html',
+                    {'band':band})
--- django/mysite/mysite/urls.py
+++ django/mysite/mysite/urls.py
@@ -19,7 +19,13 @@ from affiliation import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', views.hello),
-    path('about-us/', views.about)
+    path('bands/', views.band_list, name='band-list'),
+    path('bands/<int:id>/',views.band_detail, name='band-detail'),
+    path('about-us/', views.about),
+    path('contact-us/', views.contact, name='contact'),
+    path('bands/add/', views.band_create, name='band-create'),
+    path('bands/<int:id>/update', views.band_update, name='band-update'),
+    path('bands/<int:id>/delete', views.band_delete, name='band-delete')
 ]
+
 
